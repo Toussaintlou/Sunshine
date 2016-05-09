b85cbbd6cf550d450a9fa76607e685dade29b51f
Toussaintlou <willjuste@gmail.com>
2016-05-09 02:06:37 -0500

    Added a ShareActionProvider method on the menu bar

--- app/src/main/java/willjuste/com/sunshine/DetailedActivity.java
+++ app/src/main/java/willjuste/com/sunshine/DetailedActivity.java
@@ -3,9 +3,13 @@ package willjuste.com.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,15 +22,12 @@ public class DetailedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
 
-
-
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new DetailedFragment())
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,7 +47,7 @@ public class DetailedActivity extends AppCompatActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
+//
         return super.onOptionsItemSelected(item);
     }
 
@@ -55,7 +56,13 @@ public class DetailedActivity extends AppCompatActivity {
      */
     public static class DetailedFragment extends Fragment {
 
+        private static final String TAG = DetailedFragment.class.getSimpleName();
+        private static final String SHARE_HASHTAG = " #SunshineApp";
+
+        private String infoStr;
+
         public DetailedFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -66,12 +73,45 @@ public class DetailedActivity extends AppCompatActivity {
 
             Intent detailedActivity = getActivity().getIntent();
             if (detailedActivity != null && detailedActivity.hasExtra(detailedActivity.EXTRA_TEXT)) {
-                String infoStr = detailedActivity.getStringExtra(detailedActivity.EXTRA_TEXT);
+                infoStr = detailedActivity.getStringExtra(detailedActivity.EXTRA_TEXT);
 
                 ((TextView) rootView.findViewById(R.id.detailed_text)).setText(infoStr);
             }
 
             return rootView;
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            //Get the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            //Get the provider and hold it to set/change the share intent.
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            //Attach an intent to the ShareActionProvider
+            if (shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(TAG, "Action Provider is null");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            //This resets the hierarchy of the Intent of the new application
+            //that you're opening with to be sure the back button returns to this application
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            //Tells android what type of data is being shared
+            shareIntent.setType("text/plain");
+            //Data to input
+            shareIntent.putExtra(Intent.EXTRA_TEXT, infoStr + SHARE_HASHTAG);
+            return shareIntent;
+        }
     }
 }
+
+
